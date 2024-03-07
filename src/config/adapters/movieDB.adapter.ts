1e77717ae1480f97b4908f16e1c009897159161d
Santiago Pacini <santiagopacini@gmail.com>
2024-03-07 17:33:50 -0300

    add env

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -4,6 +4,6 @@ export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
     language: 'es',
-    api_key: '',
+    api_key: process.env.MOVIE_DB_API_KEY ?? '',
   },
 });
